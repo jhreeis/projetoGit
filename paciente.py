de59345d46943670e26fe595169323430b49ee06
unknown <reisdomenejoaohigor@gmail.com>
2024-11-26 15:53:57 -0300

    Criação completa da classe Paciente

--- paciente.py
+++ paciente.py
@@ -1,9 +1,22 @@
 from pessoa import Pessoa
 
 class Paciente(Pessoa):
-    def __init__(self, nome, idade, sexo, temperatura, pressao, peso,):
+    def __init__(self, nome, idade, sexo, temperatura, pressao, peso):
         super().__init__(nome, sexo, idade)
         self.__temperatura = temperatura
         self.__pressao = pressao
         self.__peso = peso
-        
+
+    def getTemperatura(self):
+        return self.__temperatura
+    def getPressao(self):
+        return self.__pressao
+    def getPeso(self):
+        return self.__peso
+    
+    def setTemperatura(self, temperatura):
+        self.__temperatura = temperatura
+    def setTemperatura(self, pressao):
+        self.__pressao = pressao
+    def setPeso(self, peso):
+        self.__peso = peso
